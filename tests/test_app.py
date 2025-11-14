b582cda3ee6a40552249d9dd93d1d3444a86ab08
Nicolas <gagnantnicolas64@gmail.com>
2025-11-14 11:00:08 +0100

    Test sur les filtres et commentaire sur les test précédent

--- tests/test_app.py
+++ tests/test_app.py
@@ -18,6 +18,8 @@ def test_homepage_status_code(client):
     response = client.get("/")
     assert response.status_code == 200 # Vérifie que la route / renvoie bien un rendu valide
 
+
+# Test sur l'affichage et l'éditions des vidéos
 def test_video_not_found_returns_404(client):
     # On demande une vidéo avec un ID qui n'existe pas
     response = client.get("/videos/9999")
@@ -27,3 +29,11 @@ def test_video_not_found_returns_404(client):
 def test_edit_video_not_found_returns_404(client):
     response = client.get("/videos/9999/edit")
     assert response.status_code == 404
+
+
+# Test sur les filtres
+def test_search_page_loads(client):
+    response = client.get("/videos/search")
+    
+    assert response.status_code == 200
+    assert b"Rechercher" in response.data
