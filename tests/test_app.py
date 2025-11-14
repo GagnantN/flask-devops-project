d89b469925edd2205884044426768b6be96f5353
Nicolas <gagnantnicolas64@gmail.com>
2025-11-14 10:49:15 +0100

    Creation de test pour l'affichage et l'edition de video

--- tests/test_app.py
+++ tests/test_app.py
@@ -17,3 +17,13 @@ def client():
 def test_homepage_status_code(client):
     response = client.get("/")
     assert response.status_code == 200 # Vérifie que la route / renvoie bien un rendu valide
+
+def test_video_not_found_returns_404(client):
+    # On demande une vidéo avec un ID qui n'existe pas
+    response = client.get("/videos/9999")
+
+    assert response.status_code == 404     # Le code attendu est 404
+
+def test_edit_video_not_found_returns_404(client):
+    response = client.get("/videos/9999/edit")
+    assert response.status_code == 404
