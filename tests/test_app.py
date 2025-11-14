034381b3c0a7379f8c67e0150eae2aec9042c03d
Lelelr <rea.leandre@yahoo.com>
2025-11-14 10:29:47 +0100

    Configuration fichier pour Pytest

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,13 +2,14 @@ import pytest
 import sys
 import os
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from app import app
+
 
 
 @pytest.fixture
 def client():
+    from app import app
     app.config["TESTING"] = True
     with app.test_client() as client:
         yield client
